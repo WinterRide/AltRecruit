37fbde871faef00cb6864f6ed9eceddb8b7144df
PXR <pxr2709@gmail.com>
2024-07-11 22:24:09 +0700

    template cleanup. added login page

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Recruitment App",
+  description: "A recruitment app built with Next.js",
 };
 
 export default function RootLayout({
