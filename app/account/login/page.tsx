f5359393d7668f3415643931e24282eed04482ae
AdamRezaJames <adam.james@binus.ac.id>
2024-07-12 00:26:53 +0700

    merge listing n fix img in login

--- app/account/login/page.tsx
+++ app/account/login/page.tsx
@@ -19,11 +19,11 @@ export default function Index() {
     <div className="w-[90vw] max-h-[90vh] m-auto rounded-lg border shadow lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
       <div className="hidden bg-muted lg:block">
         <Image
-          src="/placeholder.svg"
+          src="/asset/yaha.jpeg"
           alt="Image"
-          width="1920"
-          height="1080"
-          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+          width={1920}
+          height={1080}
+          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale rounded-l-md"
         />
       </div>
       <div className="flex items-center justify-center py-12">
